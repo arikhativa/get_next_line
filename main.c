bcc547f62fad4a78ae5254437a73e21b19fe2ff1
yoav rabby <yoavar@protonmail.com>
2019-04-21 16:29:48 +0200

    b

---
+++ main.c
@@ -0,0 +1,30 @@
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int main (int ac, char **av)
+{
+	int fd;
+	char **line;
+
+	if (!(line = (char**)malloc(sizeof(char*) * 5)))
+		return (0);
+	fd = open("a.txt", O_RDONLY);
+	get_next_line(fd, line);
+	printf("%s\n", *line);
+	get_next_line(fd, line);
+	printf("%s\n", *line);
+	get_next_line(fd, line);
+	printf("%s\n", *line);
+	get_next_line(fd, line);
+	printf("%s\n", *line);
+	get_next_line(fd, line);
+	printf("%s\n", *line);
+	close(fd);
+	/*while (get_next_line(fd, line) != 0)
+	{
+		printf("%s\n", *line);
+	}*/
+	return (0);
+}
